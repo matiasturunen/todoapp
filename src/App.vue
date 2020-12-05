885c94811352271c6874f68cda2ec703d8b10494
Matias Turunen <mt95@windowslive.com>
2020-12-05 11:45:27 +0200

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,86 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>
+      
+    </header>
+    <div id="categories">
+      <Category 
+        v-for="cat in categories"
+        v-bind:title="cat.name"
+        v-bind:item="cat"
+        v-bind:key="cat.id"
+      ></Category>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Category from './components/Category.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Category
+  },
+  data: function() {
+    return{
+      categories: [
+        {
+          name: "Backlog",
+          id:1,
+          items: [
+            {
+              name: "TD1-1",
+              description: "DESC1-1"
+            },
+            {
+              name: "TD1-2",
+              description: "DESC1-2"
+            },
+            {
+              name: "TD1-3",
+              description: "DESC1-3"
+            }
+          ]
+        },
+        {
+          name: "Doing",
+          id:2,
+          items: [
+          {
+              name: "TD2-1",
+              description: "DESC2-1"
+            },
+            {
+              name: "TD2-2",
+              description: "DESC2-2"
+            },
+            {
+              name: "TD2-3",
+              description: "DESC2-3"
+            }
+          ]
+        },
+        {
+          name: "Done",
+          id:3,
+          items: [
+          {
+              name: "TD3-1",
+              description: "DESC3-1"
+            },
+            {
+              name: "TD3-2",
+              description: "DESC3-2"
+            },
+            {
+              name: "TD3-3",
+              description: "DESC3-3"
+            }
+          ]
+        }
+      ]
+    }
   }
 }
 </script>
---
+++ src/components/Category.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="category">
+		<h1>{{ title }}</h1>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Category",
+	props: {
+		title: String,
+		item: Object
+	}
+}
+</script>
+
+<style >
+.category {
+	width: 300px;
+	float: left;
+	display: block;
+	background-color: #7A98EF;
+	margin: 10px;
+	min-height: 500px;
+}
+</style>
